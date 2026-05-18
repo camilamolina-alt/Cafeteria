06969763c03e92a8b421ff83a5751f8b14f18b78
Camila <camila.molina@sansano.usm.cl>
2026-05-18 10:23:08 -0400

    Carrusel y estilos, configuraciones de static listos

--- cafeteria_app/urls.py
+++ cafeteria_app/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='index'),
+    path('shop/', views.shop, name='shop'),
+    path('menu/', views.menu, name='menu'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('events/', views.events, name='events'),
+    path('login/', views.login, name='login'),
+    path('cart/', views.cart, name='cart')
 ]
--- cafeteria_app/views.py
+++ cafeteria_app/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'cafeteria_app/index.html')
+
+def shop(request):
+    return render(request, 'cafeteria_app/shop.html')
+
+def menu(request):
+    return render(request, 'cafeteria_app/menu.html')
+
+def gallery(request):
+    return render(request, 'cafeteria_app/gallery.html')
+
+def events(request):
+    return render(request, 'cafeteria_app/events.html')
+
+def login(request):
+    return render(request, 'cafeteria_app/login.html'),
+
+def cart(request):
+ return render(request, 'cafeteria_app/cart.html'),
