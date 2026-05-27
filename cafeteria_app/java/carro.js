554d5149dfeec6a1ba817940e9c1632241ce74d0
TanmareUni <fvalenzuelar@usm.cl>
2026-05-26 21:52:51 -0400

    agregue el codigo del carro

---
+++ cafeteria_app/java/carro.js
@@ -0,0 +1,59 @@
+let cart = [];
+
+function agregarAlCarro(nombre, precio){
+    const item = cart.find(producto => producto.nombre === nombre);
+    if (item){
+        item.cantidad+=1;    
+    } else {
+        cart.push({nombre, precio, cantidad:1})
+    }
+    actualizarCarro();
+}
+
+function RemoverDelCarro(nombre){
+    cart = cart.filter(producto => producto.nombre !==nombre);
+    actualizarCarro();
+}
+
+function actualizarCarro(){
+    const cartItems = document.getElementById('cart-items');
+    const cartTotal = document.getElementById('cart-total');
+    cartItems.innerHTML = '';
+    let total = 0;
+    cart.forEach(producto => {
+        const li = document.createElement('li');
+        li.textContent = '${producto.nombre} - $${producto.precio} x ${producto.cantidad}';
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Eliminar';
+        removeButton.onclick = () => removerDelCarro(producto.nombre);
+        li.appendChild(removeButton);
+        cartItems.appendChild(li);
+        total += producto.precio * producto.cantidad;
+    });
+    cartTotal.textContent = 'total: $${total}';
+}
+const pagarRequest = new pagarRequest(
+    [
+        {
+            metodoSoportado: 'Tarjeta',
+            data: {
+                tiposDeTarjeta: ['visa', 'mastercard'], 
+            },
+        },
+    ],
+    {
+        total: {
+            label: 'Total',
+            cantidad: { currency: 'CLP', value: '0.00'},
+        },
+    }
+);
+pagarRequest.show().then((pagarRespuesta) => {
+    console.log(pagarRespuesta);
+
+    pagarRespuesta.complete('Exito').then(() => {
+        console.log('Pago completado con éxito');
+    });
+}).catch((error) => {
+    console.error('Error en el pago');
+});
