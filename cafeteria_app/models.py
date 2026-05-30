70b66457a02702039d8ac16a9964bc14930a8e94
Camila <camila.molina@sansano.usm.cl>
2026-05-30 13:40:32 -0400

    ACTUALIZADO LO DE DEL FOOTER

--- cafeteria_app/models.py
+++ cafeteria_app/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=15)
+    def __str__(self):
+        return self.name
+    
 class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     price = models.PositiveSmallIntegerField() #por ahora solo por ver que ondis
     details = models.TextField(blank = True)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
     def __str__(self):
         return self.name
+
     
