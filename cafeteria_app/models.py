1ce00fe076165118b5840ab5aa952679958c01b9
Camila <camila.molina@sansano.usm.cl>
2026-06-13 22:46:34 -0400

    EL INICIO DE LA PAGINA ADMIN

--- cafeteria_app/models.py
+++ cafeteria_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+#ARCHIVOS DEL PANEL DE ADMIN 
 class Category(models.Model):
     name = models.CharField(max_length=15)
     def __str__(self):
--- cafeteria_app/urls.py
+++ cafeteria_app/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('ejemplo/', views.ejemplo, name='ejemplo'),
     path('login/', views.signin, name='login'),
     path('signup/', views.registro, name='signup'),
+    path('signup/', views.registro, name='signup'),  
     path('cart/', views.cart_detail, name='cart'),
     path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
